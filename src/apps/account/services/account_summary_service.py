e7d453e19f325c625544fbaa2b15cf69fbadc003
Saulo Emmanuel <saulocomercial7@gmail.com>
2025-11-16 12:22:50 -0300

    feat: adiciona mais métricas em DashboardView

--- src/apps/account/services/account_summary_service.py
+++ src/apps/account/services/account_summary_service.py
@@ -1,17 +1,11 @@
-from datetime import timedelta
-from django.db.models import Sum, F, Count
-from django.db.models.functions import TruncMonth
+from django.db.models import Count
 from django.utils.timezone import now
-
-from apps.transaction.models import TransactionModel
-from apps.transaction.models.choices import TypeTransactionChoices
 
 
 class AccountSummary:
     def __init__(self, queryset):
         self.queryset = queryset
         self.today = now().date()
-        self.one_year_ago = self.today.replace(day=1) - timedelta(days=365)
 
     def total_values(self):
         total_accounts = self.queryset.count()
@@ -32,79 +26,6 @@ class AccountSummary:
             'number_accounts_by_type': qs
         }
 
-
-    # def values_by_category(self):
-    #     recipes_qs = (
-    #         TransactionModel.objects
-    #         .filter(
-    #             account__in=self.queryset,
-    #             type_transaction=TypeTransactionChoices.RECIPE
-    #         )
-    #         .exclude(category__isnull=True)
-    #         .values("category__name")
-    #         .annotate(total=Sum("value"))
-    #     )
-
-    #     expenses_qs = (
-    #         TransactionModel.objects
-    #         .filter(
-    #             account__in=self.queryset,
-    #             type_transaction=TypeTransactionChoices.EXPENSE
-    #         )
-    #         .exclude(category__isnull=True)
-    #         .values("category__name")
-    #         .annotate(total=Sum("value"))
-    #     )
-
-    #     return {
-    #         "recipes_by_category": {
-    #             item["category__name"]: item["total"] or 0
-    #             for item in recipes_qs
-    #         },
-    #         "expenses_by_category": {
-    #             item["category__name"]: item["total"] or 0
-    #             for item in expenses_qs
-    #         },
-    #     }
-
-    # def get_monthly_summary(self):
-    #     recipes_qs = (
-    #         TransactionModel.objects
-    #         .filter(
-    #             account__in=self.queryset,
-    #             type_transaction=TypeTransactionChoices.RECIPE,
-    #             created_at__gte=self.one_year_ago,
-    #         )
-    #         .annotate(month=TruncMonth("created_at"))
-    #         .values("month")
-    #         .annotate(total=Sum("value"))
-    #         .order_by("month")
-    #     )
-
-    #     expenses_qs = (
-    #         TransactionModel.objects
-    #         .filter(
-    #             account__in=self.queryset,
-    #             type_transaction=TypeTransactionChoices.EXPENSE,
-    #             created_at__gte=self.one_year_ago,
-    #         )
-    #         .annotate(month=TruncMonth("created_at"))
-    #         .values("month")
-    #         .annotate(total=Sum("value"))
-    #         .order_by("month")
-    #     )
-
-    #     return {
-    #         "recipes_by_month": {
-    #             item["month"].strftime("%Y-%m"): item["total"] or 0
-    #             for item in recipes_qs
-    #         },
-    #         "expenses_by_month": {
-    #             item["month"].strftime("%Y-%m"): item["total"] or 0
-    #             for item in expenses_qs
-    #         },
-    #     }
-
     def set_response(self):
         # ** para fazer merge de dicts
         return {
@@ -113,5 +34,3 @@ class AccountSummary:
             # **self.values_by_category(),
             # **self.get_monthly_summary(),
         }
-
-# TODO: incluir o que está comentado no dashboard_view
--- src/apps/base/services/dashboard_metrics_service.py
+++ src/apps/base/services/dashboard_metrics_service.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from django.db.models import Sum
+from django.db.models.functions import TruncMonth
 
 from apps.account.services import AccountSummary
 from apps.transaction.models import TransactionModel
@@ -7,8 +9,10 @@ from apps.transaction.models.choices import TypeTransactionChoices
 # TODO: continuar trabalhando no Dashboard
 class DashboardMetrics(AccountSummary):
     def __init__(self, request, queryset):
+        super().__init__(queryset)
         self.request = request
         self.queryset = queryset
+        self.one_year_ago = self.today.replace(day=1) - timedelta(days=365)
 
     def total_values(self):
         context = super().total_values()
@@ -36,5 +40,84 @@ class DashboardMetrics(AccountSummary):
         context['total_balance'] = total_balance
         context['total_recipe'] = total_recipe
         context['total_expense'] = total_expense
+
+        return context
+
+    def values_by_category(self):
+        recipes_qs = (
+            TransactionModel.objects
+            .filter(
+                account__in=self.queryset,
+                type_transaction=TypeTransactionChoices.RECIPE
+            )
+            .exclude(category__isnull=True)
+            .values("category__name")
+            .annotate(total=Sum("value"))
+        )
+
+        expenses_qs = (
+            TransactionModel.objects
+            .filter(
+                account__in=self.queryset,
+                type_transaction=TypeTransactionChoices.EXPENSE
+            )
+            .exclude(category__isnull=True)
+            .values("category__name")
+            .annotate(total=Sum("value"))
+        )
+
+        return {
+            "recipes_by_category": {
+                item["category__name"]: item["total"] or 0
+                for item in recipes_qs
+            },
+            "expenses_by_category": {
+                item["category__name"]: item["total"] or 0
+                for item in expenses_qs
+            },
+        }
+
+    def get_monthly_summary(self):
+        recipes_qs = (
+            TransactionModel.objects
+            .filter(
+                account__in=self.queryset,
+                type_transaction=TypeTransactionChoices.RECIPE,
+                created_at__gte=self.one_year_ago,
+            )
+            .annotate(month=TruncMonth("created_at"))
+            .values("month")
+            .annotate(total=Sum("value"))
+            .order_by("month")
+        )
+
+        expenses_qs = (
+            TransactionModel.objects
+            .filter(
+                account__in=self.queryset,
+                type_transaction=TypeTransactionChoices.EXPENSE,
+                created_at__gte=self.one_year_ago,
+            )
+            .annotate(month=TruncMonth("created_at"))
+            .values("month")
+            .annotate(total=Sum("value"))
+            .order_by("month")
+        )
+
+        return {
+            "recipes_by_month": {
+                item["month"].strftime("%Y-%m"): item["total"] or 0
+                for item in recipes_qs
+            },
+            "expenses_by_month": {
+                item["month"].strftime("%Y-%m"): item["total"] or 0
+                for item in expenses_qs
+            },
+        }
+    
+    def set_response(self):
+        context = super().set_response()
+        context['values_by_category'] = self.values_by_category()
+        context['monthly_summary'] = self.get_monthly_summary()
 
         return context
