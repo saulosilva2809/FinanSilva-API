eaa04c1b64c3c13fbb761b8d6ab6cbf98fc99d08
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-12-08 09:08:42 -0300

    feat and refactror: add RecurringTransactionModel in admin and refactor TransactionAdmin

--- src/apps/transaction/admin.py
+++ src/apps/transaction/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import TransactionModel
+from .models import TransactionModel, RecurringTransactionModel
 
 
-admin.site.register(TransactionModel)
+@admin.register(TransactionModel)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('account', 'value', 'type_transaction', 'recurring_root')
+
+@admin.register(RecurringTransactionModel)
+class RecurringTransactionAdmin(admin.ModelAdmin):
+    list_display = ('account', 'value', 'type_transaction', 'init_date', 'next_run_date', 'active')
