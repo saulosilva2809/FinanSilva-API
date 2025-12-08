de704d913835460d5cf6ceda8d5334008ec7f075
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-12-08 09:09:22 -0300

    feat: created delete_celery_task_post_delete_transaction signals

--- src/apps/transaction/signals/recurring_transaction_signals.py
+++ src/apps/transaction/signals/recurring_transaction_signals.py
@@ -1,39 +1,36 @@
 import json
 
-from django.db.models.signals import post_save
+from django.db import transaction
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
-from django.utils.timezone import make_aware
 from django_celery_beat.models import ClockedSchedule, PeriodicTask
 
 from apps.transaction.models import RecurringTransactionModel
 
 
 @receiver(post_save, sender=RecurringTransactionModel)
-def update_or_create_task_celery_to_recurring_transaction(sender, instance, created, **kwargs):
+def create_task_celery_to_recurring_transaction(sender, instance, created, **kwargs):
     if created and instance.active:
-        if instance.executed_first_time == True:
-            run_date = instance.next_run_date
-        else:
-            run_date = instance.init_date
+        run_date = instance.init_date if not instance.executed_first_time else instance.next_run_date
 
-        if run_date and run_date.tzinfo is None:
-            run_date = make_aware(run_date)
+        clocked = ClockedSchedule.objects.create(
+            clocked_time=run_date
+        )
 
-        try:
-            clocked, _ = ClockedSchedule.objects.get_or_create(
-                clocked_time=run_date
-            )
+        PeriodicTask.objects.create(
+            name=f"transaction_{instance.id}",
+            task="apps.base.tasks.process_recurring_transaction_task.process_recurring_transaction",
+            clocked=clocked,
+            one_off=True,
+            args=json.dumps([str(instance.id)]),
+        )
 
-            PeriodicTask.objects.update_or_create(
-                name=f"transaction_{instance.id}",
-                defaults={
-                    "task": "apps.base.tasks.process_recurring_transaction_task.process_recurring_transaction",
-                    "clocked": clocked,
-                    "one_off": True,
-                    "enabled": True,
-                    "args": json.dumps([str(instance.id)]), 
-                }
-            )
-        
-        except Exception as e:
-            raise e
+@receiver(pre_delete, sender=RecurringTransactionModel)
+def delete_celery_task_post_delete_transaction(sender, instance, **kwargs):
+    with transaction.atomic():
+        periodic_task = PeriodicTask.objects.filter(
+            name=f'transaction_{instance.id}'
+        )
+
+        if periodic_task:
+            periodic_task.delete()
