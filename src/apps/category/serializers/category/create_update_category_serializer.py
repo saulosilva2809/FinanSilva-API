557e098c96067b4a377b5c993eadfd9f89c9be21
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-12-19 18:36:07 -0300

    chore: adiciona TODO para futura refatoração de serializer

--- src/apps/category/serializers/category/create_update_category_serializer.py
+++ src/apps/category/serializers/category/create_update_category_serializer.py
@@ -4,9 +4,11 @@ from apps.category.models import CategoryModel
 
 
 class CreateUpdateCategorySerializer(serializers.ModelSerializer):
+    # TODO: refatorar para mostrar nome da conta e id
     class Meta:
         model = CategoryModel
         fields = [
+            'id',
             'name',
             'description',
             'account',
