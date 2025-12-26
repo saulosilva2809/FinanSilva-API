d5cd430e21f73c30c271bcacba6718a0ab04cb8f
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-12-26 09:04:47 -0300

    refactor: altera BaseSerializer para suportar campos adicionais

--- src/apps/base/serializer/base_serializer.py
+++ src/apps/base/serializer/base_serializer.py
@@ -2,5 +2,8 @@ from rest_framework import serializers
 
 
 class BaseMiniSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    name = serializers.CharField()
+    def __init__(self, fields: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field_instance in fields.items():
+            self.fields[field_name] = field_instance
