715744b7372d71c1908ad518a7810e58eff4bf56
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-01-05 09:53:52 -0300

    feat: adiciona validações importantes no serializer

--- src/apps/transaction/serializers/transfer/create_update_transer_serializer.py
+++ src/apps/transaction/serializers/transfer/create_update_transer_serializer.py
@@ -1,34 +1,43 @@
 from rest_framework import serializers
 
-from apps.account.models import AccountModel
 from apps.transaction.models import TransferModel
 
 
 class CreateUpdateTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = TransferModel
-        fields = ('id', 'original_account', 'account_transferred', 'value', 'description', 'category', 'subcategory')
+        fields = (
+            'id', 'original_account', 'account_transferred', 'value', 'description', 'category', 'subcategory')
 
     def validate_value(self, value):
-        print(f'VALUE: {self.initial_data.get("value")}')
-        if value < 0:
-            raise serializers.ValidationError(
-                "O valor da transação não pode ser negativo."
-            )
-
-        original_account_id = self.initial_data.get("original_account")
-        account_transferred_id = self.initial_data.get("account_transferred")
-
-        if original_account_id == account_transferred_id:
-            raise serializers.ValidationError(
-                "A conta de origem não pode ser a mesma que a conta de destino."
-            )
-        
-        original_account_model = AccountModel.objects.get(id=original_account_id)
-
-        if value > original_account_model.balance:
-            raise serializers.ValidationError(
-                'Você não possui saldo sulficiente.'
-            )
-        
+        if value <= 0:
+            raise serializers.ValidationError("O valor da transferência deve ser maior que zero.")
         return value
+
+    def validate(self, attrs):
+        user = self.context['request'].user
+        original_account = attrs.get('original_account')
+        account_transferred = attrs.get('account_transferred')
+        value = attrs.get('value')
+
+        if original_account == account_transferred:
+            raise serializers.ValidationError({
+                'account_transferred': 'A conta de destino não pode ser a mesma que a conta de origem.'
+            })
+
+        if original_account and value and value > original_account.balance:
+            raise serializers.ValidationError({
+                'value': 'Você não possui saldo suficiente nesta conta para realizar esta transferência.'
+            })
+        
+        if original_account and original_account.user != user:
+            raise serializers.ValidationError({
+                'original_account': 'Você não tem permissão para realizar transferências a partir desta conta.'
+            })
+
+        if account_transferred and account_transferred.user != user:
+            raise serializers.ValidationError({
+                'account_transferred': 'Você só pode transferir para contas que pertencem a você.'
+            })
+
+        return attrs
