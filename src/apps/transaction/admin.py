885d1a17ba0d994491abad5d9af1b7a190836c17
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-01-09 18:52:58 -0300

    feat: Adicionar métodos de exclusão ao admin de transações

--- src/apps/transaction/admin.py
+++ src/apps/transaction/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 
 from .models import RecurringTransactionModel, TransactionModel, TransferModel
+from apps.transaction.services import TransactionService
 
 
 @admin.register(TransactionModel)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'updated_at', 'account', 'value', 'type_transaction', 'recurring_root')
+    list_display = ('created_at', 'updated_at', 'account', 'value', 'type_transaction', 'recurring_root', 'transfer_root')
+
+    # para várias exclusões
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            TransactionService.delete_transaction(obj)
+
+    # para uma única
+    def delete_model(self, request, obj):
+        TransactionService.delete_transaction(obj)
+
 
 @admin.register(RecurringTransactionModel)
 class RecurringTransactionAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'updated_at', 'account', 'value', 'type_transaction', 'init_date', 'next_run_date', 'active', 'executed_first_time', 'executed_last_time')
 
+
 @admin.register(TransferModel)
 class TransferAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'updated_at', 'original_account', 'account_transferred', 'value', 'category', 'subcategory')
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            TransactionService.delete_transfer(obj)
+
+    def delete_model(self, request, obj):
+        TransactionService.delete_transfer(obj)
