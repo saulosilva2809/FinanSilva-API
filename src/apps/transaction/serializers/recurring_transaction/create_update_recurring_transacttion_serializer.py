af2a5a632e384452ee4ac3dd0dfd70bd6c079ab6
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-01-09 18:53:08 -0300

    fix: Corrigir nome do arquivo do serializador de transação recorrente

---
+++ src/apps/transaction/serializers/recurring_transaction/create_update_recurring_transacttion_serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from apps.account.models import AccountModel
+from apps.transaction.models.choices import TypeTransactionChoices
+from apps.transaction.models import RecurringTransactionModel
+
+
+class CreateUpdateRecurringTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecurringTransactionModel
+        fields = ('id', 'account', 'value', 'type_transaction', 'description', 'frequency', 'active', 'category', 'subcategory', 'init_date')
+
+    def validate_value(self, value):
+        if value < 0:
+            raise serializers.ValidationError("O valor da transação não pode ser negativo.")
+
+        if self.initial_data.get('type_transaction') == TypeTransactionChoices.EXPENSE:
+            if value > self.initial_data.get('account').balance:
+                raise serializers.ValidationError('Você não possui saldo sulficiente.')
+
+        return value
