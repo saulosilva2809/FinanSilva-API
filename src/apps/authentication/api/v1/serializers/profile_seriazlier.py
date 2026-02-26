8afc5e934f01eca015648f611d11a219f4b63e11
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-02-26 09:02:03 -0300

    refactor: formats created_at and updated_at in the response

--- src/apps/authentication/api/v1/serializers/profile_seriazlier.py
+++ src/apps/authentication/api/v1/serializers/profile_seriazlier.py
@@ -1,9 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from apps.authentication.models import UserModel
 
 
-class ViewProfileSerializer(ModelSerializer):
+class ViewProfileSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format='%d/%m/%Y, %H:%M:%S')
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y, %H:%M:%S')
+
     class Meta:
         model = UserModel
         fields = ('first_name', 'last_name', 'email', 'created_at', 'updated_at')
